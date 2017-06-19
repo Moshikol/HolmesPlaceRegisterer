3487b3c7096fc78bf2ff45d98281583bc267acc7
moshiko lev <moshiko.lev@gmail.com>
2017-06-19 12:50:57 +0300

    Added Token Handling

--- HolmesPlaceRegisterer/Program.cs
+++ HolmesPlaceRegisterer/Program.cs
@@ -2,6 +2,8 @@
 using System.IO;
 using System.Net;
 using System.Net.Mail;
+using System.Configuration;
+using System.Xml;
 
 namespace HolmesPlaceRegisterer
 {
@@ -9,11 +11,14 @@ namespace HolmesPlaceRegisterer
     {
         static void Main(string[] args)
         {
-            string globres = "";
+            string globres = "", Token = "";
             string usrID = "10534167";
+            string LoginReq = "";
+
             Exception EX2 = new Exception();
             try
             {
+                #region old func
                 // var httpWebRequest = (HttpWebRequest)WebRequest.Create("http://api.holmesplace.co.il/MobileWebSite/Pages/Spinning.aspx/RegisterToSpinningClass");
                 //httpWebRequest.ContentType = "application/json";
                 //httpWebRequest.Method = "POST";
@@ -35,7 +40,20 @@ namespace HolmesPlaceRegisterer
                 //    var result = streamReader.ReadToEnd();
                 //    globres = result.ToString();
                 //    EmailSend(usrID, result.ToString(), "You Just Registerd Successfully", EX2);
-                //}
+                //} 
+                #endregion
+
+                using (var streamReader = new StreamReader(@"D:\Docs\ReqText.txt"))
+                {
+                    LoginReq = streamReader.ReadToEnd();
+                }
+                Stream  res = SendWebReq("POST", LoginReq, "text/xml;charset=utf-8", "http://api.holmesplace.co.il/WebServices/LoginService.asmx" , true);
+                Token = GetTokenFromReq(res);
+                //double d = ConvertToUnixTimestamp(DateTime.Now);
+                // 10078 = sigal 10079 = dudi
+                //  string json = String.Format("{{'companyId':200, 'branchId':210, 'userId':{0},'token':'72253fd0d48d4800a9372c09c6140113', 'lessonId':'10078', 'date': {1}, 'time':'191500', 'seatId':23}}", usrID, d).ToString();
+
+                //  Console.WriteLine(RESULT);
             }
             catch (Exception EX)
             {
@@ -44,42 +62,67 @@ namespace HolmesPlaceRegisterer
 
         }
 
-        public static string SendWebReq(string Method, string json, string contentType, string url)
+        public static Stream SendWebReq(string Method, string ReqContent, string contentType, string url, bool Islogin)
         {
+
             var httpWebRequest = (HttpWebRequest)WebRequest.Create(url);
             httpWebRequest.ContentType = contentType;
             httpWebRequest.Method = Method;
+            httpWebRequest.Host = "api.holmesplace.co.il";
+            httpWebRequest.ContentLength = ReqContent.Length;
+            if (Islogin)
+            {
+                httpWebRequest.UserAgent = "ksoap2-android/2.6.0+";
+                httpWebRequest.Headers.Add("USER_NAME", "sysuser!@#$");
+                httpWebRequest.Headers.Add("PASSWORD", "sysPassword!@#$");
+                httpWebRequest.Headers.Add("SYSTEM", "MOBILE");
+                httpWebRequest.ContentLength = ReqContent.Length;
+            }
+
             try
             {
 
                 using (var streamWriter = new StreamWriter(httpWebRequest.GetRequestStream()))
                 {
-                    double d = ConvertToUnixTimestamp(DateTime.Now);
-                    // 10078 = sigal 10079 = dudi
-                    //  string json = String.Format("{{'companyId':200, 'branchId':210, 'userId':{0},'token':'72253fd0d48d4800a9372c09c6140113', 'lessonId':'10078', 'date': {1}, 'time':'191500', 'seatId':23}}", usrID, d).ToString();
 
-                    streamWriter.Write(json);
+
+                    streamWriter.Write(ReqContent);
                     streamWriter.Flush();
                     streamWriter.Close();
                 }
 
-                var httpResponse = (HttpWebResponse)httpWebRequest.GetResponse();
-                using (var streamReader = new StreamReader(httpResponse.GetResponseStream()))
-                {
-                    var result = streamReader.ReadToEnd();
+                HttpWebResponse httpResponse = (HttpWebResponse)httpWebRequest.GetResponse();
+                
 
-                    EmailSend("", result.ToString(), "You Just Registerd Successfully", null);
-                    return result.ToString();
-                }
+                    Stream result = httpResponse.GetResponseStream();
+
+                    //   EmailSend("", result.ToString(), "You Just Registerd Successfully", null);
+                    // Console.WriteLine("The Responed of the Server is : " + result.ToString());
+                    return result;
+              
             }
             catch (Exception ex)
             {
-
-                return ex.Message;
+                Console.WriteLine(ex.Message);
+                // return ex.Message;
+                return null;
             }
 
 
 
+        }
+        public static string GetTokenFromReq(Stream Req)
+        {
+            string Token = "";
+            XmlDocument xd = new XmlDocument();
+            xd.Load(Req);
+            XmlNodeList RelNodes = xd.GetElementsByTagName("Token");
+           Token = RelNodes.Item(0).InnerText;
+
+
+
+
+            return Token;
         }
         public static double ConvertToUnixTimestamp(DateTime date)
         {
